9d4a87f5a6cba644eac4f2cdc7898390e7db2f0f
Santiago Dobón <santifinland@gmail.com>
2021-11-01 15:27:54 +0100

    [FEATURE] Move train

--- metro/src/app/app.module.ts
+++ metro/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrainComponent } from './train/train.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TrainComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
